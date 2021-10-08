4b8ef37791627d452ae2b32e91ee4cc92988009b
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-10-08 10:14:40 -0300

    adding the validation endpoint

---
+++ src/middlewares/jwt-authentication.middleware.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import ForbiddenError from "../models/errors/forbidden.error.model";
+import JWT from 'jsonwebtoken';
+
+async function jwtAuthenticationMiddleware(req: Request, res: Response, next: NextFunction){
+
+    try {
+
+        const authorizationHeader = req.headers["authorization"];       
+    
+        if(!authorizationHeader){
+            throw new ForbiddenError("Credencias não informadas");
+        }
+
+        const [authorizationType, token] = authorizationHeader.split(' ');
+
+        if(authorizationType != "Bearer" || !token){
+            throw new ForbiddenError("Tipo de autenticação inválido");
+        }
+
+        try {
+            const tokenPayload = JWT.verify(token, "my_secret_key");
+    
+            
+            if(typeof tokenPayload !== "object" || !tokenPayload.sub){
+                throw new ForbiddenError("Token inválido");
+            }
+    
+            
+            
+            const user = {
+                uuid: tokenPayload.sub,
+                username: tokenPayload.username
+    
+            }
+            req.user = user;
+            next();
+        } catch (error) {
+            throw new ForbiddenError("Token Inválido");
+        }
+
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+export default jwtAuthenticationMiddleware;
--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -4,6 +4,7 @@ import userRespository from "../repositories/user.respository";
 import JWT from "jsonwebtoken";
 import { StatusCodes } from "http-status-codes";
 import basicAuthenticationMiddleware from "../middlewares/basic-authentication.middleware";
+import jwtAuthenticationMiddleware from "../middlewares/jwt-authentication.middleware";
 
 
 const authorizationRoute = Router();
@@ -30,6 +31,11 @@ authorizationRoute.post("/token", basicAuthenticationMiddleware ,async (req: Req
 
     
 
+});
+
+authorizationRoute.post("/token/validate", jwtAuthenticationMiddleware, (req: Request, res: Response, next: NextFunction) => {
+    
+    res.sendStatus(StatusCodes.OK);
 });
 
 export default authorizationRoute;
